bcf90a721a436d778264ea2aa5f43e8abfd6afa5
leongjunjie13 <69500257+leongjunjie13@users.noreply.github.com>
2020-12-28 11:54:07 +0800

    Add files via upload

---
+++ Snake.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author LeongJunJie
+ */
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+public class Snake extends JFrame {
+
+    public Snake() {
+        
+        initUI();
+    }
+    
+    private void initUI() {
+        
+        add(new Board());
+               
+        setResizable(false);
+        pack();
+        
+        setTitle("Snake");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    
+
+    public static void main(String[] args) {
+        
+        EventQueue.invokeLater(() -> {
+            JFrame ex = new Snake();
+            ex.setVisible(true);
+        });
+    }
+
+            
+}
+
